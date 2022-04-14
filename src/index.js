2fb878ccef2791d098bda38aa34a623e7040695f
dinton harold <dintonharold@gmail.com>
2022-04-14 22:50:33 +0300

    modification on using map method to render props

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import Navbar from "./components/navbar";
 import Hero from "./components/heroSection";
 import Card from "./components/card";
 // importing array with iems that we shall use the map method
-import data from "./components/data"
+import data from "./components/data";
 // console.log(data);
 
 // array to store
-const dataElements= data.map(item => {
+const dataElements = data.map((item) => {
   return (
     <Card
+      key={item.id}
       image={item.image}
       figure={item.figure}
       rating={item.rating}
@@ -22,15 +23,14 @@ const dataElements= data.map(item => {
       location={item.location}
     />
   );
-})
+});
 ReactDOM.render(
-  
   <React.StrictMode className="container mx-auto ">
     <Navbar image="airbnb.png" />
     <Hero image="Group77.png" />
     {/* When passing images as props, we only include the name of the image and not the file path */}
     {/* when passing non string values as props, we put them in braces with no */}
-    {dataElements}
+    <section className="container mx-auto flex">{dataElements}</section>
   </React.StrictMode>,
   document.getElementById("root")
 );
